f8a3a8bef1791ca3a24173ef19a2d129fbb7f08f
meharpatel2512@gmail.com <meharpatel2512@gmail.com>
2025-05-14 08:28:24 +0530

    About

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import CustomCursor from '../Components/CustomCursor'
 import Navbar from '../Components/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import { Home } from 'lucide-react'
+import Home from '../Pages/Home'
+import NotFound from '../Pages/NotFound'
 import { ResumeQues } from '../Pages/ResumeQues'
 import { ResumePreviewPage } from '../Components/ResumePreviewPage'
 import Footer from '../Components/Footer'
 import Try from '../Pages/Try'
+import AboutPage from '../Pages/AboutPage'
+import ContactPage from '../Pages/ContactPage'
 
 const App = () => {
   return (
@@ -16,8 +19,11 @@ const App = () => {
       <Routes>
         <Route path='/home' element={<Try />} />
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contact' element={<ContactPage />} />
         <Route path='/resumecreator' element={<ResumeQues />} />
         <Route path='/resumepreview' element={<ResumePreviewPage />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
