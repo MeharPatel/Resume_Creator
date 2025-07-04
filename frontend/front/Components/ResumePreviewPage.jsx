abff487cdfa7927a9b3efe455bc2fbb991e5b634
mehar.patel <mehar.patel@srkay.com>
2025-07-04 17:20:43 +0530

    Template 2

--- frontend/front/Components/ResumePreviewPage.jsx
+++ frontend/front/Components/ResumePreviewPage.jsx
@@ -1,108 +1,142 @@
-import { useState } from 'react';
-import { templates } from '../src/util/templates';
-import { ResumePreview } from '../Pages/ResumePreview';
+// import { useState } from 'react';
+// import { templates } from '../src/util/templates';
+// import { ResumePreview1 } from '../Pages/ResumePreview1';
+// import { motion } from 'framer-motion';
+
+// export const ResumePreviewPage = () => {
+//   const [formData, setFormData] = useState({
+//   firstName: 'Mehar',
+//   lastName: 'Patel',
+//   professional_title: 'Backend Developer',
+//   professional_description: 'Backend Developer for 1+ years',
+//   email: 'csmeharpatel@gmail.com',
+//   phone: '5987654321',
+//   address: 'Nanpura',
+//   city: 'Surat',
+//   state: 'Gujarat',
+//   zip_code: '395001',
+//   country: 'India',
+//   education: [
+//     {
+//       school: 'RNGPIT',
+//       degree: 'B.E.',
+//       field_of_study: 'Computer Science & Engineering',    
+//       start_date: '2020-08',
+//       end_date: '2024-05',
+//       description: 'Bachelor of Engineering with ml'
+//     },
+//     {
+//       school: "St. Xavier's High School",
+//       degree: 'HSC',
+//       field_of_study: 'HSC',
+//       start_date: '2018-07',
+//       end_date: '2020-05'
+//     },
+//     {
+//       school: 'Lourdes Convent School',
+//       degree: 'SSC',
+//       field_of_study: 'SSC',
+//       start_date: '2017-05',
+//       end_date: '2018-03'
+//     }
+//   ],
+//   experience: [
+//     {
+//       company: 'SRKAY Consulting Group',
+//       position: 'Backend Developer',
+//       start_date: '2024-05',
+//       end_date: '',
+//       current: 'on',
+//       description: 'Working as Backend Developer'
+//     },
+//     {
+//       company: 'SRKAY Consulting Group',
+//       position: 'Backend Developer',
+//       start_date: '2024-05',
+//       end_date: '',
+//       current: 'on',
+//       description: 'Working as Backend Developer'
+//     }
+//   ],
+//   skills: [{name : 'Javascript', level : 'Beginner'} , { name : 'CSS', level : "rdtfyg"} , {name : 'HTML', level : 'gftydrtyfugi' } , { name : 'ReactJs', level : 'yutryrtsdfgfui'} , {name : 'NodeJs', level: 'trfugijygutf'}, {name : 'Javascript', level : 'Beginner'} , { name : 'CSS', level : "rdtfyg"} , {name : 'HTML', level : 'gftydrtyfugi' } , { name : 'ReactJs', level : 'yutryrtsdfgfui'} , {name : 'NodeJs', level: 'trfugijygutf'}, {name : 'Javascript', level : 'Beginner'} , { name : 'CSS', level : "rdtfyg"} , {name : 'HTML', level : 'gftydrtyfugi' } , { name : 'ReactJs', level : 'yutryrtsdfgfui'} , {name : 'NodeJs', level: 'trfugijygutf'}],
+//   achievements: [ { title: 'Leetcode Solved 600+ Problems' } ,{ title : 'Heyyyyooooooooo'}],
+//   projects: [
+//     {
+//       title: 'Portfolio',
+//       projectUrl: 'meharpatel.netlify.app',
+//       description: 'Portfolio Project'
+//     },
+//     {
+//       title: 'Portfolio',
+//       projectUrl: 'meharpatel.netlify.app',
+//       description: 'Portfolio Project'
+//     }
+//   ],
+//   socialLinks: {
+//     github: 'https://github.com/MeharPatel2512',
+//     linkedin: 'https://linkedin.com/MeharPatel2512',
+//     instagram: 'https://instagram.com/MeharPatel',
+//     facebook: '',
+//     portfolio: 'meharpatel.netlify.app'
+//   },
+//   otherSocialLinks: [{name: "Some1", link: "liguebdsvgftfrdftgyuh"}, {name: "Som2", link: "uyrsdfxgcyutfghiyfg"}],
+//   customSections: [ { sectionName: 'Hobbies', items: ['Singing', 'Sketching'] },
+//                     { sectionName: 'Something', items: ['Item1', 'Item2'] } ]
+// });
+
+//   const [selectedTemplate, setSelectedTemplate] = useState('professional'); // Default template
+
+//   return (
+//     <div className='relative pt-32 pb-20 md:py-40 overflow-hidden'> 
+
+//       <div className="mb-8 text-center">
+//         <h2 className="text-2xl font-bold gradient-text mb-4">Preview</h2>
+//         <p className="text-gray-600">
+//           Here's a preview of your resume. You can go back to make changes.
+//         </p>
+//       </div>
+
+//       {/* {templates.singlepage} */}
+
+//       {/* Template Selector */}
+//       <div className="flex gap-4 mb-8 justify-center flex-wrap">
+//         {Object.keys(templates.multipage).map((templateKey) => (
+//           <motion.button
+//             key={templateKey}
+//             whileHover={{ scale: 1.05 }}
+//             whileTap={{ scale: 0.95 }}
+//             onClick={() => setSelectedTemplate(templateKey)}
+//             className={`px-4 py-2 rounded-sm capitalize ${
+//               selectedTemplate === templateKey
+//                 ? "bg-blue-600 text-white"
+//                 : "bg-gray-200 text-gray-800"
+//             }`}
+//           >
+//             {templateKey}
+//           </motion.button>
+//         ))}
+//       </div>
+
+
+
+//       {/* Pass the selected template down */}
+//       <ResumePreview1
+//         formData={formData} 
+//         selectedTemplate={selectedTemplate} 
+//         setSelectedTemplate = {setSelectedTemplate}
+//       />
+//     </div>
+//   );
+// }
+
+
+
+import React from 'react'
 
 export const ResumePreviewPage = () => {
-  const [formData, setFormData] = useState({
-  firstName: 'Mehar',
-  lastName: 'Patel',
-  professional_title: 'Backend Developer',
-  professional_description: 'Backend Developer for 1+ years',
-  email: 'csmeharpatel@gmail.com',
-  phone: '5987654321',
-  address: 'Nanpura',
-  city: 'Surat',
-  state: 'Gujarat',
-  zip_code: '395001',
-  country: 'India',
-  education: [
-    {
-      school: 'RNGPIT',
-      degree: 'B.E.',
-      field_of_study: 'Computer Science & Engineering',    
-      start_date: '2020-08',
-      end_date: '2024-05',
-      description: 'Bachelor of Engineering with ml'
-    },
-    {
-      school: "St. Xavier's High School",
-      degree: 'HSC',
-      field_of_study: 'HSC',
-      start_date: '2018-07',
-      end_date: '2020-05'
-    },
-    {
-      school: 'Lourdes Convent School',
-      degree: 'SSC',
-      field_of_study: 'SSC',
-      start_date: '2017-05',
-      end_date: '2018-03'
-    }
-  ],
-  experience: [
-    {
-      company: 'SRKAY Consulting Group',
-      position: 'Backend Developer',
-      start_date: '2024-05',
-      end_date: '',
-      current: 'on',
-      description: 'Working as Backend Developer'
-    },
-    {
-      company: 'SRKAY Consulting Group',
-      position: 'Backend Developer',
-      start_date: '2024-05',
-      end_date: '',
-      current: 'on',
-      description: 'Working as Backend Developer'
-    }
-  ],
-  skills: [{name : 'Javascript', level : 'Beginner'} , { name : 'CSS', level : "rdtfyg"} , {name : 'HTML', level : 'gftydrtyfugi' } , { name : 'ReactJs', level : 'yutryrtsdfgfui'} , {name : 'NodeJs', level: 'trfugijygutf'}, {name : 'Javascript', level : 'Beginner'} , { name : 'CSS', level : "rdtfyg"} , {name : 'HTML', level : 'gftydrtyfugi' } , { name : 'ReactJs', level : 'yutryrtsdfgfui'} , {name : 'NodeJs', level: 'trfugijygutf'}, {name : 'Javascript', level : 'Beginner'} , { name : 'CSS', level : "rdtfyg"} , {name : 'HTML', level : 'gftydrtyfugi' } , { name : 'ReactJs', level : 'yutryrtsdfgfui'} , {name : 'NodeJs', level: 'trfugijygutf'}],
-  achievements: [ { title: 'Leetcode Solved 600+ Problems' } ,{ title : 'Heyyyyooooooooo'}],
-  projects: [
-    {
-      title: 'Portfolio',
-      projectUrl: 'meharpatel.netlify.app',
-      description: 'Portfolio Project'
-    },
-    {
-      title: 'Portfolio',
-      projectUrl: 'meharpatel.netlify.app',
-      description: 'Portfolio Project'
-    }
-  ],
-  socialLinks: {
-    github: 'https://github.com/MeharPatel2512',
-    linkedin: 'https://linkedin.com/MeharPatel2512',
-    instagram: 'https://instagram.com/MeharPatel',
-    facebook: '',
-    portfolio: 'meharpatel.netlify.app'
-  },
-  otherSocialLinks: [{name: "Some1", link: "liguebdsvgftfrdftgyuh"}, {name: "Som2", link: "uyrsdfxgcyutfghiyfg"}],
-  customSections: [ { sectionName: 'Hobbies', items: ['Singing', 'Sketching'] },
-                    { sectionName: 'Something', items: ['Item1', 'Item2'] } ]
-});
-
-  const [selectedTemplate, setSelectedTemplate] = useState('professional'); // Default template
-
   return (
-    <div className='relative pt-32 pb-20 md:py-40 overflow-hidden'> 
-
-      <div className="mb-8 text-center">
-        <h2 className="text-2xl font-bold gradient-text mb-4">Preview</h2>
-        <p className="text-gray-600">
-          Here's a preview of your resume. You can go back to make changes.
-        </p>
-      </div>
-
-
-
-      {/* Pass the selected template down */}
-      <ResumePreview 
-        formData={formData} 
-        selectedTemplate={selectedTemplate} 
-        setSelectedTemplate = {setSelectedTemplate}
-      />
+    <div>
+      Mehar
     </div>
-  );
+  )
 }
---
+++ frontend/front/Pages/ResumePreview2.jsx
@@ -0,0 +1,262 @@
+// import { templates } from '../src/util/templates';
+// import { usePDF } from 'react-to-pdf';
+// import { motion } from 'framer-motion';
+// import { HomeIcon, BriefcaseBusiness, AtSign, Phone, GraduationCap, Building2, Award, ChartPie, FolderHeart, Star, Github, Linkedin, Instagram, Facebook, User, Signature } from 'lucide-react';
+
+// export const ResumePreview2 = ({ formData, selectedTemplate, setSelectedTemplate }) => {
+//   const { toPDF, targetRef } = usePDF({
+//     filename: `${formData.firstName}_${formData.lastName}_Resume.pdf`,
+//     page: {
+//       margin: { top: 15, right: 0, bottom: 15, left: 0 }, // Consistent margins in mm
+//       format: 'a4',
+//     },
+//     resolution: 2,
+//     method: open
+//   });
+
+//   const template = templates.singlepage[selectedTemplate];
+
+//   return (
+//     <div className="p-8 bg-gray-50 min-h-screen">
+
+//       {/* Resume Preview */}
+//       <div className="flex flex-col lg:flex-row gap-6 justify-center items-center">
+//         <motion.div
+//           key={selectedTemplate}
+//           initial={{ opacity: 0, y: 10 }}
+//           animate={{ opacity: 1, y: 0 }}
+//           transition={{ duration: 0.5 }}
+//           ref={targetRef}
+//           className={`${template.resume_container} pdf-container`}
+//         >
+//           <h1 className={template.name}>
+//             <>
+//               {formData.firstName} {formData.lastName}
+//             </>
+//           </h1>
+
+//           {/* Contact Info */}
+//           <section className={`${template.itemSubtitle}`}>
+//             <div className="flex">
+//               <span className="h-4 w-4 me-2 mt-2">
+//                 <AtSign className={template.custom_icons} />
+//               </span>
+//               {formData.email && <span> {formData.email} | </span>}
+//               <span className="h-4 w-4 mx-2 mt-2">
+//                 <Phone className={template.custom_icons} />
+//               </span>
+//               {formData.phone && <span>{formData.phone}</span>}
+//             </div>
+
+//             <div className="flex">
+//               <span className="h-4 w-4 me-2 mt-2">
+//                 <BriefcaseBusiness className={template.custom_icons} />
+//               </span>
+//               <p className={template.itemSubtitle}>
+//                 {formData.professional_title} | {formData.professional_description}
+//               </p>
+//             </div>
+
+//             <div className="flex">
+//               <span className="h-4 w-4 me-2 mt-2">
+//                 <HomeIcon className={template.custom_icons} />
+//               </span>
+//               <p className={template.itemSubtitle}>
+//                 {formData.address}, {formData.city}, {formData.state}, {formData.country} - {formData.zip_code}
+//               </p>
+//             </div>
+//           </section>
+
+//           {/* Education */}
+//           {formData.education.length > 0 && (
+//             <section className="page-break">
+//               <h2 className={template.sectionTitle}>Education</h2>
+//               {formData.education.map((edu, index) => (
+//                 <div key={index} className="mt-2">
+//                   <div className="flex">
+//                     <span className="h-5 w-5 me-2 mt-3">
+//                       <GraduationCap className={template.custom_icons} />
+//                     </span>
+//                     <h3 className={template.itemTitle}>{edu.degree} ({edu.field_of_study})</h3>
+//                     <span className={`${template.itemSubtitle} ms-2 mt-1`}>
+//                       | {edu.start_date} - {edu.end_date}
+//                     </span>
+//                   </div>
+//                   <p>
+//                     <span className={template.itemSubtitle}>{edu.school} </span>
+//                     {edu.description && <span className={template.itemDescription}> | {edu.description}</span>}
+//                   </p>
+//                 </div>
+//               ))}
+//             </section>
+//           )}
+
+//           {/* Experience */}
+//           {formData.experience.length > 0 && (
+//             <section className="page-break">
+//               <h2 className={template.sectionTitle}>Experience</h2>
+//               {formData.experience.map((exp, index) => (
+//                 <div key={index} className="mt-2">
+//                   <div className="flex">
+//                     <span className="h-5 w-5 me-2 mt-3">
+//                       <Building2 className={template.custom_icons} />
+//                     </span>
+//                     <h3 className={template.itemTitle}>{exp.company}</h3>
+//                     <span className={`${template.itemSubtitle} mt-1 ms-2`}>
+//                       | {exp.start_date} - {exp.end_date ? exp.end_date : 'Present'}
+//                     </span>
+//                   </div>
+//                   <p>
+//                     <span className={template.itemSubtitle}> {exp.position} </span> | <span className={template.itemDescription}>{exp.description}</span>
+//                   </p>
+//                 </div>
+//               ))}
+//             </section>
+//           )}
+
+//           {/* Skills */}
+//           {formData.skills.length > 0 && (
+//             <section className="page-break">
+//               <h2 className={template.sectionTitle}>Skills</h2>
+//               <div className="flex flex-wrap gap-2">
+//                 {formData.skills.map((skill, index) => (
+//                   <span key={index} className={template.skill}>
+//                     <span>{skill.name} - {skill.level} </span>
+//                   </span>
+//                 ))}
+//               </div>
+//             </section>
+//           )}
+
+//           {/* Achievements */}
+//           {formData.achievements.length > 0 && (
+//             <section className="page-break">
+//               <h2 className={template.sectionTitle}>Achievements</h2>
+//               {formData.achievements.map((achievement, index) => (
+//                 <div key={index} className="mt-2 flex">
+//                   <span className="h-4 w-4 me-2 mt-2">
+//                     <Award className={template.custom_icons} />
+//                   </span>
+//                   <h3 className={template.itemSubtitle}>{achievement.title}</h3>
+//                 </div>
+//               ))}
+//             </section>
+//           )}
+
+//           {/* Project */}
+//           {formData.projects.length > 0 && (
+//             <section className="page-break">
+//               <h2 className={template.sectionTitle}>Projects</h2>
+//               {formData.projects.map((proj, index) => (
+//                 <div key={index} className="mt-2">
+//                   <div className="flex">
+//                     <span className="h-4 w-4 me-2 mt-3">
+//                       <Star className={template.custom_icons} />
+//                     </span>
+//                     <h3 className={template.itemTitle}>{proj.title}</h3>
+//                     {proj.projectUrl.length > 0 && (
+//                       <span className={`${template.itemSubtitle} ms-2 mt-1`}> | {proj.projectUrl}</span>
+//                     )}
+//                   </div>
+//                   <p className={template.itemDescription}>{proj.description}</p>
+//                 </div>
+//               ))}
+//             </section>
+//           )}
+
+//           {/* Social Links */}
+//           <section className="page-break">
+//             <div className="grid grid-cols-2 gap-0 mt-2">
+//               {formData.socialLinks.github.length > 0 && (
+//                 <div className="flex">
+//                   <span className="h-4 w-4 me-2 mt-3">
+//                     <Github className={template.custom_icons} />
+//                   </span>
+//                   <h3 className={template.itemSubtitle}>{formData.socialLinks.github}</h3>
+//                 </div>
+//               )}
+//               {formData.socialLinks.linkedin.length > 0 && (
+//                 <div className="flex">
+//                   <span className="h-4 w-4 me-2 mt-3">
+//                     <Linkedin className={template.custom_icons} />
+//                   </span>
+//                   <h3 className={template.itemSubtitle}>{formData.socialLinks.linkedin}</h3>
+//                 </div>
+//               )}
+//               {formData.socialLinks.instagram.length > 0 && (
+//                 <div className="flex">
+//                   <span className="h-4 w-4 me-2 mt-3">
+//                     <Instagram className={template.custom_icons} />
+//                   </span>
+//                   <h3 className={template.itemSubtitle}>{formData.socialLinks.instagram}</h3>
+//                 </div>
+//               )}
+//               {formData.socialLinks.facebook.length > 0 && (
+//                 <div className="flex">
+//                   <span className="h-4 w-4 me-2 mt-3">
+//                     <Facebook className={template.custom_icons} />
+//                   </span>
+//                   <h3 className={template.itemSubtitle}>{formData.socialLinks.facebook}</h3>
+//                 </div>
+//               )}
+//               {formData.socialLinks.portfolio.length > 0 && (
+//                 <div className="flex">
+//                   <span className="h-4 w-4 me-2 mt-3">
+//                     <Signature className={template.custom_icons} />
+//                   </span>
+//                   <h3 className={template.itemSubtitle}>{formData.socialLinks.portfolio}</h3>
+//                 </div>
+//               )}
+//               {formData.otherSocialLinks.map((soc, index) => (
+//                 <div key={index} className={`${template.custom_icons} break-after`}>
+//                   <span className={template.itemSubtitle}>{soc.name}</span> {" "}
+//                   <span className={template.itemSubtitle}>{soc.link}</span>
+//                 </div>
+//               ))}
+//             </div>
+//           </section>
+
+//           {/* Custom Sections */}
+//           <div>
+//             {formData.customSections.map((section, sectionIndex) => (
+//               <div key={sectionIndex} className={`${template.customSection} page-break`}>
+//                 <h2 className={template.customSectionTitle}>{section.sectionName}</h2>
+//                 <div className="flex flex-wrap gap-2">
+//                   {section.items.map((item, itemIndex) => (
+//                     <span key={itemIndex} className={template.customSectionItems}>
+//                       <span>{item}</span>
+//                     </span>
+//                   ))}
+//                 </div>
+//               </div>
+//             ))}
+//           </div>
+//         </motion.div>
+//       </div>
+
+//       {/* Download Button */}
+//       <div className="flex justify-center mt-8">
+//         <button
+//           onClick={toPDF}
+//           className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
+//         >
+//           Download PDF
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+
+import React from 'react'
+
+const ResumePreview2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ResumePreview2
